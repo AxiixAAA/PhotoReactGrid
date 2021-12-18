322cdb5d7876741da4c4a86557333a6475cef5b9
AxiixAAA <slavae22@mail.ru>
2021-12-18 18:40:43 +0500

    111

--- src/components/Sidebar/SidebarOpen/SidebarOpen.jsx
+++ src/components/Sidebar/SidebarOpen/SidebarOpen.jsx
@@ -5,7 +5,13 @@ export const SidebarOpen = () =>{
 
     return<>
         <div className={s.SidebarOpenContainet}>
-
+            <div className={s.MenuContainer}>
+                <div>Все обои</div>
+                <div>Обои на телефон</div>
+                <div>Обои на рабочий стол</div>
+                <div>Красивые картинки</div>
+                <div>Обои для девочек</div>
+            </div>
         </div>
     </>
 }
