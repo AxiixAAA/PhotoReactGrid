67ca5c3388c75a029abdf85abe246abd7a969d31
AxiixAAA <slavae22@mail.ru>
2021-12-25 11:21:20 +0500

    save

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 // import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 // import { Provider } from "react-redux";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             {/* <Provider store={store}> */}
                 <App />
             {/* </Provider> */}
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
   document.getElementById('root')
 );
